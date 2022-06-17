ff1eb478b4559173fc77b8547da6a4caf311e054
luizdopc <luizotaviosoares@outlook.com>
2022-06-17 17:08:28 -0300

    adds screens

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import {ActivityIndicator, SafeAreaView, StyleSheet, View} from 'react-native';
+
+import {CatCard, CatCardProps} from '../components/CatCard';
+import {SwitchTabs} from '../components/SwitchTabs';
+import {useGetCatData} from '../hooks/useGetCatData';
+import {voteService} from '../services';
+
+export const HomeScreen = () => {
+  const [currentActive, setCurrentActive] = useState<'left' | 'right'>('left');
+
+  const handleSwipeLeft = async (item: CatCardProps) => {
+    await voteService(item.image.id, 0);
+  };
+
+  const handleSwipeRight = async (item: CatCardProps) => {
+    await voteService(item.image.id, 1);
+  };
+
+  const {catData, isLoading} = useGetCatData();
+  return (
+    <SafeAreaView style={{backgroundColor: '#FBFAFE'}}>
+      <View style={styles.container}>
+        <SwitchTabs
+          onPressLeft={() => {
+            setCurrentActive('left');
+          }}
+          onPressRight={() => {
+            setCurrentActive('right');
+          }}
+          currentActive={currentActive}
+        />
+
+        {isLoading ? (
+          <View style={{height: 450, justifyContent: 'center'}}>
+            <ActivityIndicator />
+          </View>
+        ) : (
+          <View style={{width: '100%', height: '75%'}}>
+            <CatCard {...catData[0]} />
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FBFAFE',
+    height: '100%',
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
+  animatedContainer: {
+    width: '100%',
+    height: '60%',
+  },
+  nextItemContainer: {
+    ...StyleSheet.absoluteFillObject,
+    marginHorizontal: 20,
+    top: 50,
+  },
+});
