31355f9e98456268946ca98309cd82623f96be5f
luizdopc <luizotaviosoares@outlook.com>
2022-06-17 17:01:32 -0300

    adds assets

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+export const Assets = {
+  chat: require('./chat.png'),
+  close: require('./close.png'),
+  heart: require('./heart.png'),
+  paws: require('./paws.png'),
+  star: require('./star.png'),
+  tinder: require('./tinder.png'),
+  user: require('./user.png'),
+  like: require('./like.png'),
+  dislike: require('./dislike.png'),
+};
